f7c391a31fbacffd651f78c60a63c76f9cf9e4aa
brandon68 <67684399+brandon68@users.noreply.github.com>
2025-01-01 11:36:15 -0600

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-# start.sh
 #!/bin/bash
 python3 botv2.py &  # Ejecuta el bot en segundo plano
-bash ping.sh      # Ejecuta el auto-ping
-
+while true; do
+    sleep 300  # Mantiene el proceso vivo, ajustable según tus necesidades
+done
